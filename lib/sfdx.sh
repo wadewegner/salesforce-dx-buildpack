5773213fe324d45334b6143dc322bac1a9691592
Wade Wegner <wade.wegner@gmail.com>
2018-02-10 15:26:08 -0800

    Updated

---
+++ lib/sfdx.sh
@@ -0,0 +1,8 @@
+# <SFDX_AUTH_URL> <flag d|s> <alias>
+auth() {
+  
+  SFDX_AUTH_URL_FILE="$BUILD_DIR/sfdxurl"
+  echo "$1" > $SFDX_AUTH_URL_FILE
+  sfdx force:auth:sfdxurl:store -f $SFDX_AUTH_URL_FILE -$2 -a $3
+
+}
