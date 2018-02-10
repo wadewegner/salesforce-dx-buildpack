03fdf774069c4b36729c06084636e71a74abdb19
Wade Wegner <wade.wegner@gmail.com>
2018-02-10 15:18:15 -0800

    Config vars available in release

--- lib/release.sh
+++ lib/release.sh
@@ -5,20 +5,6 @@ source <(curl -s --retry 3 https://lang-common.s3.amazonaws.com/buildpack-stdlib
 
 header "Running release.sh"
 
-BUILD_DIR=${1:-}
-CACHE_DIR=${2:-}
-ENV_DIR=${3:-}
-BP_DIR=$(cd $(dirname ${0:-}); cd ..; pwd)
-
-log "Exporting environment directories ..."
-export_env "$ENV_DIR"
-
-log "BUILD_DIR $BUILD_DIR"
-log "CACHE_DIR $CACHE_DIR"
-log "ENV_DIR $ENV_DIR"
-log "BP_DIR $BP_DIR"
-
-
 log "Config vars ..."
 log "-- DEV_HUB_SFDX_AUTH_URL: $DEV_HUB_SFDX_AUTH_URL"
 log "-- STAGE: $STAGE"
