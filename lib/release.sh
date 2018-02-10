409e07b038ca83595d50e91d968bff07cbd307d1
Wade Wegner <wade.wegner@gmail.com>
2018-02-10 15:07:38 -0800

    Updated

--- lib/release.sh
+++ lib/release.sh
@@ -1,5 +1,6 @@
 
 source common.sh
+source <(curl -s --retry 3 https://lang-common.s3.amazonaws.com/buildpack-stdlib/v7/stdlib.sh)
 
 header "Running release.sh"
 
@@ -7,6 +8,9 @@ BUILD_DIR=${1:-}
 CACHE_DIR=${2:-}
 ENV_DIR=${3:-}
 BP_DIR=$(cd $(dirname ${0:-}); cd ..; pwd)
+
+log "Exporting environment directories ..."
+export_env "$ENV_DIR"
 
 log "BUILD_DIR $BUILD_DIR"
 log "CACHE_DIR $CACHE_DIR"
