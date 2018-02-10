95fddfd7c5aa2d82c9b84bb62e8abc954527d49c
Wade Wegner <wade.wegner@gmail.com>
2018-02-10 05:46:09 -0800

    Updating to use .salesforcedx.yaml

--- lib/common.sh
+++ lib/common.sh
@@ -10,6 +10,22 @@ export_env_dir() {
   fi
 }
 
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
 header() {
   echo "" || true
   echo -e "-----> \e[34m$*\033[0m" || true
