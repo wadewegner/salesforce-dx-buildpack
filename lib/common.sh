c8476d652ef408509303d790e170caf1677f50b5
Wade Wegner <wade.wegner@gmail.com>
2018-02-10 06:13:05 -0800

    Fixed for dashs in yml names

--- lib/common.sh
+++ lib/common.sh
@@ -12,12 +12,17 @@ export_env_dir() {
 
 parse_yaml() {
    local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   local s='[[:space:]]*'
+   local w='[a-zA-Z0-9_]*'
+   local fs=$(echo @|tr @ '\034')
+   
    sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s,-,_,g" \
         -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
    awk -F$fs '{
       indent = length($1)/2;
       vname[indent] = $2;
+
       for (i in vname) {if (i > indent) {delete vname[i]}}
       if (length($3) > 0) {
          vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
