5b4e6911d7a2d0fbf4ce147af5394b8c444883cd
Wade Wegner <wade.wegner@gmail.com>
2018-02-10 15:06:25 -0800

    Adding commands to release.sh

--- lib/release.sh
+++ lib/release.sh
@@ -1,2 +1,14 @@
 
-echo "hello"
+source common.sh
+
+header "Running release.sh"
+
+BUILD_DIR=${1:-}
+CACHE_DIR=${2:-}
+ENV_DIR=${3:-}
+BP_DIR=$(cd $(dirname ${0:-}); cd ..; pwd)
+
+log "BUILD_DIR $BUILD_DIR"
+log "CACHE_DIR $CACHE_DIR"
+log "ENV_DIR $ENV_DIR"
+log "BP_DIR $BP_DIR"
