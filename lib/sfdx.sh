3f3c53ae0f28c977f4e32664fd12edb752335712
Wade Wegner <wade.wegner@gmail.com>
2018-02-13 04:13:29 -0800

    Fixed bug on auth method

--- lib/sfdx.sh
+++ lib/sfdx.sh
@@ -3,7 +3,7 @@
 # <DIR> <SFDX_AUTH_URL> <d|s> <alias>
 auth() {
   
-  SFDX_AUTH_URL_FILE="$1/sfdxurl"
+  SFDX_AUTH_URL_FILE="$1"
   if [ ! "$2" == "" ]; then
     echo "$2" > "$SFDX_AUTH_URL_FILE"
   fi
